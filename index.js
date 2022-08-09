03ee1802e1fd71d4fc3f3ad6b5a4376c51dd33b4
Jemaul Hunter <jemaulhunter@gmail.com>
2022-08-09 11:23:59 -0400

    commit tree acfdd7b6834c2e9974f740b80adebcdf93828522

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+//remove main
+mainElement.remove();
+//create a newHeader variable. create an h1 = victory(id)
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Jemaul is the champion!';
+document.body.append(newHeader);
+
+
